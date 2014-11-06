3da190646ef93d1cb18ea4f6471daa4dd6243d5f
May Yang <mayyang@May-Yangs-MacBook-Pro.local>
2014-11-06 15:41:25 -0800

    Initial Commit

---
+++ FavoritePhotosChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FavoritePhotosChallenge
+//
+//  Created by May Yang on 11/6/14.
+//  Copyright (c) 2014 May Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
